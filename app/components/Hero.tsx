a4dc6c0d21798e441c8641b90cfc64a909246e81
Jakub Grzymisławski <thegomel10@gmail.com>
2024-06-02 22:22:12 +0200

    Add sass and framer-motion, Container component, create Navigation component

---
+++ app/components/Hero.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import FeaturedProducts from '../grid/FeaturedProducts';
+
+const Hero = () => {
+  return <div>{/* <FeaturedProducts /> */}</div>;
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import styles from './page.module.css';
 
-import Hero from '@/app/components/layout/Hero';
+import Hero from '@/app/components/Hero';
 
 export default function Home() {
   return (
